cf56bf3fa0e8776ea92b14243706c8e3da36cea9
miorantsoa <miorantsioaRak@gmail.com>
2017-05-20 20:45:47 +0300

    Modification BDD

--- application/views/accueil.php
+++ application/views/accueil.php
@@ -1,3 +1,94 @@
-<?= $content?>
-
+<div class="section">
+    <div class="video-bg">
+        <video poster="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/polina.jpg" id="bgvid" playsinline autoplay muted loop>
+<!--           WCAG general accessibility recommendation is that media such as background video play through only once. Loop turned on for the purposes of illustration; if removed, the end of the video will fade in the same way created by pressing the "Pause" button  -->
+            <source src="http://thenewcode.com/assets/videos/polina.webm" type="video/webm">
+            <source src="http://thenewcode.com/assets/videos/polina.mp4" type="video/mp4">
+        </video>
+    </div>
+</div>
+<div class="section">
+    <div class="price-tag">
+        <div class="pricing">
+            <div class="[ price-option price-option--low ]">
+                <div class="price-option__detail">
+                    <span class="price-option__cost">$0</span>
+                    <span class="price-option__type">Freeuser</span>
+                </div>
+                <a href="#" class="price-option__purchase">Réserver</a>
+            </div>
+            <div class="[ price-option price-option--mid ]">
+                <div class="price-option__detail">
+                    <span class="price-option__cost">$20</span>
+                    <span class="price-option__type">Poweruser</span>
+                </div>
+                <a href="#" class="price-option__purchase">Réserver</a>
+            </div>
+            <div class="[ price-option price-option--high ]">
+                <div class="price-option__detail">
+                    <span class="price-option__cost">$40</span>
+                    <span class="price-option__type">Superuser</span>
+                </div>
+                <a href="#" class="price-option__purchase">Réserver</a>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="section">
+    <div class="gg-attraction">
+        <div class="gg-title">
+            <div class="gg-wiered-border">
+                <div class="gg-box-title">
+                    <h2>Découvrir nos Univers</h2>
+                    <p class="gg-subtitle">
+                        Venez découvrir des sensation inoubliable
+                    </p>
+                </div>
+            </div>
+            <img src="<?= base_url('assets/img/tour-icon1.png') ?>" alt="Illustration titre découvrir">
+        </div>
+        <div class="container">
+            <div class="col-md-4 col-sm-4 col-xs-12">
+                <div class="gg-box gg-mg">
+                    <div class="gg-btn">
+                        <div class="gg-btn btn btn-primary btn-lg">
+                            Découvrir
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-4 col-xs-12">
+                <div class="gg-box gg-primal">
+                    <div class="gg-btn">
+                        <div class="gg-btn btn btn-primary btn-lg">
+                            Découvrir
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-4 col-xs-12">
+                <div class="gg-box gg-guerre">
+                    <div class="gg-btn">
+                        <div class="gg-btn btn btn-primary btn-lg">
+                            Découvrir
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="gg-attraction gg-album">
+        <div class="gg-title">
+            <div class="gg-wiered-border">
+                <div class="gg-box-title">
+                    <h2>Découvrir nos Univers</h2>
+                    <p class="gg-subtitle">
+                        Venez découvrir des sensation inoubliable
+                    </p>
+                </div>
+            </div>
+            <img src="<?= base_url('assets/img/tour-icon1.png') ?>" alt="Illustration titre découvrir">
+        </div>
+    </div>
+</div>
     
