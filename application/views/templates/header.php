f2c8fe7c641fc1e720fc442506ed6318b14013bd
miorantsoa <miorantsioaRak@gmail.com>
2017-05-21 00:46:15 +0300

    Supprimer nav-fixed - Mike

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -15,6 +15,7 @@
     <!--Custom Css-->
     <link rel="stylesheet" href="<?= base_url('assets/css/style.css')?>">
     <link rel="stylesheet" href="<?= base_url('assets/css/gg-style.css')?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/mike.css')?>">
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
