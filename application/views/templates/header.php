2c1a829d4461086b8fa40841e0bea8ed97b8f671
miorantsoa <miorantsioaRak@gmail.com>
2017-05-21 08:03:34 +0300

    Description modifier

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title>Accueil</title>
+    <title><?= $titre?></title>
     <link rel="stylesheet" href="<?= base_url('assets/css/bootstrap.min.css')?>" type="text/css">
     <!-- Custom Fonts -->
     <link href='https://fonts.googleapis.com/css?family=Mrs+Sheppards%7CDosis:300,400,700%7COpen+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800;' rel='stylesheet' type='text/css'>
